1d3b26eebe87c3e4515c575edf1deed605091f63
suzutails <suzusuzuch@gmail.com>
2025-11-02 20:53:49 -0400

    Test Character and Movement Set

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    private Rigidbody2D rb;
+    private Vector2 moveInput;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        moveInput = Vector2.zero;
+
+        if (Input.GetKey(KeyCode.W)) moveInput.y += 1;
+        if (Input.GetKey(KeyCode.S)) moveInput.y -= 1;
+        if (Input.GetKey(KeyCode.A)) moveInput.x -= 1;
+        if (Input.GetKey(KeyCode.D)) moveInput.x += 1;
+
+        moveInput = moveInput.normalized; // Keep speed consistent diagonally
+    }
+
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + moveInput * moveSpeed * Time.fixedDeltaTime);
+    }
+}
